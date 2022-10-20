110b8cd46898ac407c6174c8adfc63844ef8fd79
UncleSparky277 <codycruz277@gmail.com>
2022-10-20 15:57:52 -0500

    "project update"

--- project1/src/main/java/dev/cruz/controllers/EmployeeController.java
+++ project1/src/main/java/dev/cruz/controllers/EmployeeController.java
@@ -1,5 +1,12 @@
 package dev.cruz.controllers;
 
+import com.google.gson.Gson;
+import dev.cruz.driver.Drive;
+import dev.cruz.entities.Employee;
+import io.javalin.http.Handler;
+
+import java.util.List;
+
 public class EmployeeController {
 
   public Handler createEmployee = (ctx) ->{
--- project1/src/main/java/dev/cruz/repositories/EmployeeDAO.java
+++ project1/src/main/java/dev/cruz/repositories/EmployeeDAO.java
@@ -1,4 +1,21 @@
 package dev.cruz.repositories;
 
+import dev.cruz.entities.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+
+    //create
+    Employee createEmployee(Employee employee);
+
+    //read
+    Employee getEmployeeById(int id);
+    List<Employee> getAllEmployees();
+
+    //update
+    Employee updateEmployee(Employee employee);
+
+    //delete
+    boolean deleteEmployeeById(int id);
 }
